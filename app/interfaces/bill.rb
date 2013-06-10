9d33fbb493be99ca37d6d9dc718a0eb07bda9233
Matheus Mina <mfbmina@gmail.com>
2013-06-10 20:47:36 -0300

    Created model bill

--- app/interfaces/bill.rb
+++ app/interfaces/bill.rb
@@ -1,3 +1,5 @@
 class Bill < ActiveRecord::Base
-  attr_accessible :expiration_date, :interest, :kind, :pay_date, :penalty, :responsible, :value
+  attr_accessible :expiration_date, :interest, :kind, :pay_date, :penalty, :value
+
+  validates_presence_of :expiration_date, :kind, :value
 end
