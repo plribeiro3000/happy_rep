2bee8baa4b7719f9f865600d9554cd4ce9f40028
Matheus Mina <mfbmina@gmail.com>
2013-06-29 15:23:43 -0300

    Fixed views for employess

--- db/migrate/20130629173728_create_employees.rb
+++ db/migrate/20130629173728_create_employees.rb
@@ -3,15 +3,10 @@ class CreateEmployees < ActiveRecord::Migration
     create_table :employees do |t|
       t.string :name
       t.string :address
-      t.date :birth_date
       t.integer :work_card
-      t.text :functions
       t.integer :register
-      t.text :experience
       t.date :admission_date
       t.float :salary
-      t.string :photo
-      t.text :about
       t.string :phone
       t.date :demission_date
 
