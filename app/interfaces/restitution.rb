5a57872830d0fb4b5268c75dc7ce5d109f417ffe
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-06-29 16:16:52 -0300

    Adding validations

--- app/interfaces/restitution.rb
+++ app/interfaces/restitution.rb
@@ -1,3 +1,7 @@
 class Restitution < ActiveRecord::Base
   attr_accessible :date, :item, :problem, :solution, :value
+
+  validates :item, :presence => true
+  validates :date, :presence => true
+  validates :value, :presence => true
 end
