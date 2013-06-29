2122c90a4163eb52a25020a90694a1ba1803ad8d
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-06-29 17:59:11 -0300

    Initial Authorization added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,16 +4,43 @@ class Ability
   def initialize(user)
     user ||= User.new
     user_accessor = UserAccessor.new(user)
-    profile = user_accessor.profile
+    profile_accessor = user_accessor.profile
 
-    if profile.admin?
-      can :manage, :all
-    elsif profile.resident?
-      # Resident Permissions
-    elsif profile.former?
-      # Former Permissions
-    elsif profile.aggregate?
-      # Aggregate Permissions
+    if profile_accessor.admin?
+      can :manage, :animal
+      can :manage, :bill
+      can :manage, :book
+      can :manage, :employee
+      can :manage, :event
+      can :manage, :loan, :profile_id => profile_accessor.id
+      can :manage, :personal_bill, :profile => profile_accessor.id
+      can :manage, :profile, :id => profile_accessor.id
+      can [ :create, :destroy ], :profile, :role => %w(resident former aggregate)
+      can :manage, :restitution
+      can :manage, :user, :id => user.id
+      can :manage, :vacine
+    elsif profile_accessor.resident?
+      can :manage, :animal
+      can :manage, :bill
+      can :manage, :book
+      can :manage, :employee
+      can :manage, :event
+      can :manage, :loan, :profile_id => profile_accessor.id
+      can :manage, :personal_bill, :profile => profile_accessor.id
+      can :manage, :profile, :id => profile_accessor.id
+      can [ :create, :destroy ], :profile, :role => %w(former aggregate)
+      can :manage, :restitution
+      can :manage, :user, :id => user.id
+      can :manage, :vacine
+    elsif profile_accessor.former?
+      can :manage, :loan, :profile_id => profile_accessor.id
+      can :manage, :personal_bill, :profile => profile_accessor.id
+      can :manage, :profile, :id => profile_accessor.id
+      can :manage, :user, :id => user.id
+    elsif profile_accessor.aggregate?
+      can :read, :event
+      can :manage, :profile, :id => profile_accessor.id
+      can :manage, :user, :id => user.id
     end
   end
 end
