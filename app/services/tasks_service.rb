7310953fd9099c8fb9aa7f692ede75f1f0efac4a
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-07-03 00:16:12 -0300

    Added Logic to keep Tasks particular to each user

--- app/services/tasks_service.rb
+++ app/services/tasks_service.rb
@@ -4,4 +4,27 @@ module TasksService
   def advanced_search(name, date)
     Task.by_name(name).by_start_date(date)
   end
+
+  def find(id)
+    { :task => Task.where(:profile_id => current_user.profile.id).find(id) }
+  end
+
+  def generate(attributes = {})
+    task = Task.new(attributes)
+    task.profile = current_user.profile
+    task.save
+    { :task => task }
+  end
+
+  def modify(id, attributes = {})
+    task = self.find(id)[:task]
+    task.update_attributes(attributes)
+    { :task => task }
+  end
+
+  def delete(id)
+    task = self.find(id)[:task]
+    task.destroy
+    { :task => task }
+  end
 end
