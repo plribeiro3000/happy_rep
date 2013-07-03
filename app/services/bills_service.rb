016802b07de8f773d2ef867dae2a541a5c61a9f5
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-07-03 03:12:22 -0300

    Protecting Personal bills

--- app/services/bills_service.rb
+++ app/services/bills_service.rb
@@ -2,6 +2,31 @@ module BillsService
   include Zertico::Service
 
   def advanced_search(expiration_date, payment_date)
-    Bill.by_expiration_date(expiration_date).by_payment_date(payment_date)
+    Bill.where(:profile_id => [ nil, current_user.id ]).by_expiration_date(expiration_date).by_payment_date(payment_date)
+  end
+
+  def build
+    { :bill => Bill.new }
+  end
+
+  def find(id)
+    { :bill => Bill.where(:profile_id => [ nil, current_user.id ]).find(id) }
+  end
+
+  def generate(attributes = {})
+    bill = Bill.create(attributes)
+    { :bill => bill, :location => bills_path }
+  end
+
+  def modify(id, attributes = {})
+    bill = self.find(id)[:bill]
+    bill.update_attributes(attributes)
+    { :bill => bill, :location => bills_path }
+  end
+
+  def delete(id)
+    bill = self.find(id)[:bill]
+    bill.destroy
+    { :bill => bill, :location => bills_path }
   end
 end
