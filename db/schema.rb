062ec84e508fecaf302eb19d6db0d5933f63c091
Matheus Mina <mfbmina@gmail.com>
2013-07-03 00:20:23 -0300

    Fixed layout

--- db/schema.rb
+++ db/schema.rb
@@ -123,11 +123,11 @@ ActiveRecord::Schema.define(:version => 20130703030707) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
