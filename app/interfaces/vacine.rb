fdc081bd634e1bb5784c12cbcb2fadbcb5025a10
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-07-04 14:38:03 -0300

    Fix for validations and view

--- app/interfaces/vacine.rb
+++ app/interfaces/vacine.rb
@@ -1,7 +1,8 @@
 class Vacine < ActiveRecord::Base
-  attr_accessible :event_date
-
-  validates :event_date, :presence => true
+  attr_accessible :event_date, :animal_id
 
   belongs_to :animal
+
+  validates :event_date, :presence => true
+  validates :animal, :presence => true
 end
